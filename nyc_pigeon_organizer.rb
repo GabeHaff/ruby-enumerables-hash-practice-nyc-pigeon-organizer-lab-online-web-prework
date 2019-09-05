3354b4987c135565f09a82ec64d7a61a864ce1d4
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 21:03:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  # write your code here!	 
+  bird_hash = {}
+   data.each do |keys, values|
+     values.each do |k2, v2|
+       v2.each do |name|
+         if !bird_hash[name]
+           bird_hash[name] = {}
+         end
+         if !bird_hash[name][keys]
+           bird_hash[name][keys]=[]
+         end
+         bird_hash[name][keys] << k2.to_s
+
+       end
+    end
+  end
+
+   bird_hash
+
+ end	
