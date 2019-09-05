4d7f125b811cecb060db1c363bf9ab434bb8f9ff
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 20:47:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,27 @@ def nyc_pigeon_organizer(data)
  # look through data again and see if each pigeon is associated with a particular attribute
  # if they are, add that attribute to their nested hash description
  # if not, move to the next attribute 
+organized_pigeons = {}
 
+  data.each do |attribute, values|
+   values.each do |key, pigeons|
+     pigeons.each do |pigeon|
+       organized_pigeons[pigeon] = {color: [], gender: [], lives: []}
+      end
+    end
+  end
+
+   organized_pigeons.each do |pigeon| 
+    data.each do |attribute, values|
+      values.each do |pair|
+        if pair[1].include?(pigeon[0])
+          organized_pigeons[pigeon[0]][attribute].push("#{pair[0]}")
+        end 
+      end
+    end
+  end
+
+   organized_pigeons
+
+ end  
 end	
