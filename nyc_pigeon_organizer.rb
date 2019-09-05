62a97afb2f99c78aefe2590179e5d67af64b27fe
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 20:43:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
-        # puts name
         if !new_hash[name]
           new_hash[name] = {}
         end
@@ -13,6 +12,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name][key] = [new_value.to_s]
           else
           new_hash[name][key] << new_value.to_s
+  }
         end
       end
     end
