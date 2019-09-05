a35ccf8ace2a1f4d3e3f040fd6302bd405403033
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 20:45:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {
+# look inside the data and create a hash of all pigeons inside. 
+ # look through data again and see if each pigeon is associated with a particular attribute
+ # if they are, add that attribute to their nested hash description
+ # if not, move to the next attribute 
 
 end	
